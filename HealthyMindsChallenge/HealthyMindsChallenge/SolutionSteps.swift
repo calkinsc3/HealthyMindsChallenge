1058e317bfe220cc00aabf235c6ae70ac500d03f
Bill Calkins <bill@calkinsconsulting.com>
2018-10-28 06:55:52 -0500

    First commit of starter project.

---
+++ HealthyMindsChallenge/HealthyMindsChallenge/SolutionSteps.swift
@@ -0,0 +1,30 @@
+//
+//  SolutionSteps.swift
+//  HealthyMindsChallenge
+//
+//  Created by William Calkins on 10/28/18.
+//  Copyright © 2018 Calkins Computer Consulting. All rights reserved.
+//
+
+import UIKit
+
+class SolutionSteps: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
