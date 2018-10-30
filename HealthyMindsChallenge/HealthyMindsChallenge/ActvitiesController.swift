a71e1e53b221e25009d8db533eb5edb0564b1e8e
Bill Calkins <bill@calkinsconsulting.com>
2018-10-29 19:55:55 -0500

    Added an activities controller.

--- HealthyMindsChallenge/HealthyMindsChallenge/ActvitiesController.swift
+++ HealthyMindsChallenge/HealthyMindsChallenge/ActvitiesController.swift
@@ -1,5 +1,5 @@
 //
-//  SeriesController.swift
+//  ActvitiesController.swift
 //  HealthyMindsChallenge
 //
 //  Created by William Calkins on 10/29/18.
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class SeriesController: UITableViewController {
+class ActvitiesController: UITableViewController {
     
-    var seriesData : [Series]?
-    private var selectedIndex : IndexPath?
+    var actvities : [Activity]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,46 +31,31 @@ class SeriesController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.seriesData?.count ?? 0
+        return self.actvities?.count ?? 0
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let givenSeriesCell = tableView.dequeueReusableCell(withIdentifier: "seriesCell", for: indexPath) as? UserDataCell,
-            let givenSeriesData = self.seriesData?[indexPath.row] else {
+        guard let givenActivityCell = tableView.dequeueReusableCell(withIdentifier: "activityCell", for: indexPath) as? UserDataCell,
+            let givenActivityData = self.actvities?[indexPath.row] else {
                 return UITableViewCell()
         }
         
-       givenSeriesCell.dataTitle.text = givenSeriesData.title
+       givenActivityCell.dataTitle.text = givenActivityData.uuid
         
-        return givenSeriesCell
+        return givenActivityCell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //record selected index
-        self.selectedIndex = indexPath
-        //deselect row
-        tableView.deselectRow(at: indexPath, animated: true)
-        //call segue
-        self.performSegue(withIdentifier: "actvitiesSegue", sender: tableView)
-    }
-    
-    
+
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "actvitiesSegue" {
-            if let activityController = segue.destination as? ActvitiesController,
-                let selectedIndex = self.selectedIndex,
-                let givenActivities = self.seriesData?[selectedIndex.row].activities {
-                
-                activityController.actvities = givenActivities
-            }
-        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-
+    */
 
 }
